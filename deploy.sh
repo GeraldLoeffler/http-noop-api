bbdcf1262e5fc0d27e5b60a45f5b930164d404f7
GeraldLoeffler <gerald.loeffler@googlemail.com>
2018-10-19 20:02:18 +0200

    last last

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,13 @@
 
 set +x
 
-# so that aliases work
-shopt -s expand_aliases
-
 export ANYPOINT_USERNAME="$1"
 export ANYPOINT_PASSWORD="$2"
 export ANYPOINT_ENVIRONMENT="$3"
 export MULE_APP="$4"
 
-# shortcut to invoke Anypoint CLI
-#alias cli="anypoint-cli --username=$ANYPOINT_USERNAME --password=$ANYPOINT_PASSWORD --environment=$ANYPOINT_ENVIRONMENT"
-
 echo Deploying $MULE_APP
 
-anypoint-cli --username=$ANYPOINT_USERNAME --password=$ANYPOINT_PASSWORD --environment=$ANYPOINT_ENVIRONMENT api-mgr api list -o json
+anypoint-cli api-mgr api list -o json
 
 set -x
