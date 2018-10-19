52809152ddfe5d2596d4f98ab9bac17d19491702
GeraldLoeffler <gerald.loeffler@googlemail.com>
2018-10-19 18:19:43 +0200

    fixing placeholder invocation of Anypoint CLI

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 set +x
 
-alias anypoint-cli='docker run --rm --name anypoint-cli -it -e ANYPOINT_USERNAME="$(echo $ANYPOINT_USERNAME)" -e ANYPOINT_PASSWORD="$(echo $ANYPOINT_PASSWORD)" integrational/anypoint-cli:3.0.0'
+export ANYPOINT_USERNAME="$1"
+export ANYPOINT_PASSWORD="$2"
 
-export ANYPOINT_USERNAME=$1
-export ANYPOINT_PASSWORD=$2
+# shortcut to invoke Anypoint CLI
+cli="docker run --rm --name anypoint-cli -it -e ANYPOINT_USERNAME=$ANYPOINT_USERNAME -e ANYPOINT_PASSWORD=$ANYPOINT_PASSWORD integrational/anypoint-cli:3.0.0"
 
 # set name to 'http-noop-api-1.0.0' or similar
 name=$(mvn help:evaluate -Dexpression=project.build.finalName | grep "^[^\[]")
@@ -13,9 +14,8 @@ name=$(mvn help:evaluate -Dexpression=project.build.finalName | grep "^[^\[]")
 # set fullName to 'target/http-noop-api-1.0.0-mule-application.jar' or similar
 fullName=$(ls target/$name*)
 
-echo Deploying $fullName with AP credentials $ANYPOINT_USERNAME and $ANYPOINT_PASSWORD
-echo TODO
+echo Deploying $fullName
 
-anypoint-cli --environment=Staging api-mgr api list -o json
+$cli --environment=Staging api-mgr api list -o json
 
 set -x
