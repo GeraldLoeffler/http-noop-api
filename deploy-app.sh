be267e8d53d35bebdaea55db842f11d48ce79699
GeraldLoeffler <gerald.loeffler@googlemail.com>
2018-10-21 14:08:06 +0200

    refactoring 5

--- deploy-app.sh
+++ deploy-app.sh
@@ -8,10 +8,17 @@ export ANYPOINT_ENV="$3"
 export APP_ARCHIVE_FILENAME="$4" # relative to target directory!
 export APP_NAME="$5"
 
-echo "Deploying Mule app $APP_ARCHIVE_FILENAME as $APP_NAME to $ANYPOINT_ENV"
+echo Deploying Mule app $APP_ARCHIVE_FILENAME as $APP_NAME to $ANYPOINT_ENV
+
+# evals to 1 if app already exists, 0 otherwise
+appExists=$(anypoint-cli runtime-mgr cloudhub-application describe $APP_NAME -o text -f Domain | grep $APP_NAME | wc -l)
+# modify all if it exists, otherwise deploy it
+command=$(if [ $appExists == 1 ]; then echo modify; else echo deploy; fi)
+
+echo Using $command command to deploy $APP_NAME
 
 cd target # because APP_ARCHIVE_FILENAME is relative to target directory
 
-anypoint-cli runtime-mgr cloudhub-application deploy \
+anypoint-cli runtime-mgr cloudhub-application $command \
             --runtime 4.1.4 --workers 2 --workerSize 0.1 --region us-east-1 --autoRestart true \
             $APP_NAME $APP_ARCHIVE_FILENAME
