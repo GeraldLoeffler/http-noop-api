9670f553ca90733659fc3379dfc6270afb39f5ba
GeraldLoeffler <gerald.loeffler@googlemail.com>
2018-10-22 09:17:32 +0200

    cleaning up again

--- artifact-final-name.sh
+++ artifact-final-name.sh
@@ -3,9 +3,8 @@
 #
 # returns something like http-noop-api-1.0.0
 #
-# must be run after another Maven build, so that Maven dependencies 
-# have already been downloaded (because the output related to downloading
-# Maven dependencies is not filtered-out and would be returned as well)
-#
+
+# dry-run to download all dependencies
+mvn help:evaluate -Dexpression=project.build.finalName &> /dev/null
 
 mvn help:evaluate -Dexpression=project.build.finalName | grep "^[^[]"
